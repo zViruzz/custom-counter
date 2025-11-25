b14bcbe9571ed3b44959a1616b72902bcf013a30
zviruzz <nahuelsinka@gmail.com>
2025-11-25 07:49:30 -0300

    style: update

--- src/components/CloseButton.jsx
+++ src/components/CloseButton.jsx
@@ -4,10 +4,10 @@ export default function CloseButton({ handleClickClose }) {
 	return (
 		<button
 			type='button'
-			className='absolute right-5 top-5 border-2 rounded-xl p-[2px] text-red-500 opacity-60 cursor-pointer hover:bg-red-500'
+			className='absolute right-5 top-5 border-2 rounded-xl p-[2px] text-red-500 opacity-80 cursor-pointer hover:bg-red-500 group'
 			onClick={handleClickClose}
 		>
-			<CloseIcon className='hover:text-black' />
+			<CloseIcon className='group-hover:text-black' />
 		</button>
 	)
 }
